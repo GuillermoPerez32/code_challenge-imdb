83f202fd5ca70b37fbc2dd1e6f4f79a54d596db5
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-07-25 10:38:37 -0400

    fix grid

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -5,16 +5,14 @@ import { MovieCard } from "./MovieCard";
 const MovieList = () => {
   const { data } = useGetPopularQuery("");
   return (
-    <Box padding={8} justifyContent="center" alignItems="center">
+    <Box padding={8}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <div>
-          {data &&
-            data.results?.map((movie) => (
-              <Grid item xs={4} key={movie.id}>
-                <MovieCard movie={movie} />
-              </Grid>
-            ))}
-        </div>
+        {data &&
+          data.results?.map((movie) => (
+            <Grid item xs={4} key={movie.id}>
+              <MovieCard movie={movie} />
+            </Grid>
+          ))}
       </Grid>
     </Box>
   );
