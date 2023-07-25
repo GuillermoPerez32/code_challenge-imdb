a2229723badb67873d6f3fa85fd2ee2c2d08153d
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-07-25 01:36:12 -0400

    fix service

--- src/services/imdb/imdb.ts
+++ src/services/imdb/imdb.ts
@@ -1,11 +1,12 @@
 // Need to use the React-specific entry point to allow generating React hooks
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { MovieDetail, MovieList } from '../../types/movie'
+import { imdb_host } from '../../constants/imdb'
 
 // Define a service using a base URL and expected endpoints
 export const imdbApi = createApi({
   reducerPath: 'imdbApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: imdb_host }),
   endpoints: (builder) => ({
     getMovieDetail: builder.query<MovieDetail, string>({
       query: () => `movie/popular`,
