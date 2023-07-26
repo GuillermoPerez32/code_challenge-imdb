c67b6336838f2c565e442d05eea06c99c31fb1b0
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-07-25 22:00:55 -0400

    scroll on click

--- src/components/MovieList/MovieCard.tsx
+++ src/components/MovieList/MovieCard.tsx
@@ -14,9 +14,13 @@ interface Props {
 }
 
 export const MovieCard = ({ movie, onClick }: Props) => {
+  const handleClick = () => {
+    onClick();
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
   return (
     <Card sx={{ minWidth: 200, maxWidth: 500 }}>
-      <CardActionArea onClick={() => onClick()}>
+      <CardActionArea onClick={handleClick}>
         <CardMedia
           component="img"
           height="140"
